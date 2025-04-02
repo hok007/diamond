e78688670356c28c5d3560d66591dfc3450b8ad7
Oeung Chheanghok <chheanghok_o@kubotacambodia.com>
2025-04-02 16:02:56 +0700

    change position of close detail

--- index.php
+++ index.php
@@ -1,29 +1,29 @@
 <?php
-require 'db.php';
+    require 'db.php';
 
-$productLists = [];
-$sql = "SELECT c.category_name, p.code, p.name, p.description, p.image, p.price
-        FROM categories c INNER JOIN products p ON c.category_id = p.category_id 
-        ORDER BY c.category_id ASC, p.product_id ASC";
-$result = $conn->query($sql);
+    $productLists = [];
+    $sql = "SELECT c.category_name, p.code, p.name, p.description, p.image, p.price
+            FROM categories c INNER JOIN products p ON c.category_id = p.category_id 
+            ORDER BY c.category_id ASC, p.product_id ASC";
+    $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
-        $categoryName = $row['category_name'];
-        if (!isset($productLists[$categoryName])) {
-            $productLists[$categoryName] = [];
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $categoryName = $row['category_name'];
+            if (!isset($productLists[$categoryName])) {
+                $productLists[$categoryName] = [];
+            }
+            $productLists[$categoryName][] = [
+                "code" => $row['code'],
+                "name" => $row['name'],
+                "description" => $row['description'],
+                "image" => $row['image'],
+                "price" => $row['price']
+            ];
         }
-        $productLists[$categoryName][] = [
-            "code" => $row['code'],
-            "name" => $row['name'],
-            "description" => $row['description'],
-            "image" => $row['image'],
-            "price" => $row['price']
-        ];
     }
-}
 
-$conn->close();
+    $conn->close();
 ?>
 
 <!DOCTYPE html>
@@ -134,7 +134,7 @@ $conn->close();
 
             Swal.fire({
                 html: 
-                    `<div class="fixed p-4">
+                    `<div class="absolute top-0 right-0 p-4">
                         <svg xmlns="http://www.w3.org/2000/svg" onclick="Swal.close()" class="h-6 w-6 text-red-500 cursor-pointer border-2 border-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                         </svg>
